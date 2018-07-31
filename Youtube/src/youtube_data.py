3f80d54dc9cdadb781f15b4a73443f33a2b4d742
jeongseok <jeongseok912@gmail.com>
2018-08-01 08:44:54 +0900

    code refactoring

--- Youtube/src/youtube_data.py
+++ Youtube/src/youtube_data.py
@@ -26,12 +26,9 @@ def getSocialerusRanking():
             # Explicit Waits
             # WebDriverWait는 성공적으로 반환될 때까지 0.5초마다 ExpectedCondition을 호출한다.
             # ExpectedCondition의 정상 리턴값 : true or not null
-
-            # TODO - 네트워크 예외 처리
             # 3초 내에 반환 요소를 못찾으면 3초 기다린 후 TimeoutException을 던짐
             btn = WebDriverWait(chromeBrowser, 3).until(EC.presence_of_element_located((By.ID, 'btnMore')))
             if chromeBrowser.find_element_by_id('btnMore').get_attribute('style') != '':
-                print(btn)
                 break
             else:
                 btn.click()
@@ -101,35 +98,36 @@ class Channel():
 ############################################################################################################################
 # Main
 ############################################################################################################################
+startTime = time.time()
 # 몽고DB connection
 conn = pymongo.MongoClient('localhost', 27017)
 db = conn.get_database('youtube_channel')
 collection = db.get_collection('channel')
 
-# 채널 객체
-ch = Channel()
-
-'''
 # 소셜러스 구독자 랭킹에서 데이터 가져오기
-print('### 소셜러스 구독자 랭킹에서 데이터 가져오기 ###')
-startTime = time.time()
+print('\n### 소셜러스 구독자 랭킹에서 데이터 가져오기 ###')
 rankingList = getSocialerusRanking()
-endTime = time.time()
-print('경과 시간 : ' + str(round((endTime-startTime)/60, 2)) + '분')
+point1 = time.time()
+print('%s seconds' %  (point1 - startTime))
 print('유튜버 수 : ' + str(len(rankingList)) + '명')
 
 # MongoDB에 랭킹 데이터 저장
+print('\n### 몽고DB에 랭킹데이터 저장하기 ###')
 for i in range(len(rankingList)-1):
     collection.insert_one(
         {"CID": rankingList[i][0],
          "Category": rankingList[i][1]})
-'''
+point2 = time.time()
+print('%s seconds' %  (point1 - point2))
+print('total %s seconds' %  (point1 - startTime))
 
 # 채널 타이틀 가져오기
+print('\n### 채널 타이틀 가져오기 ###')
 selectAll = collection.find({}, no_cursor_timeout=True) # return type: cursor
 start = time.time()
 t = 0
 cnt = 0
+ch = Channel()
 for selectOne in selectAll:
     ch.setChannelID(selectOne['CID'])
     channelURL = 'https://www.youtube.com/channel/' + ch.getChannelID()
@@ -137,14 +135,12 @@ for selectOne in selectAll:
     collection.update({'_id': selectOne['_id']}, {'$set': {'ChannelName': ch.getChannelTitle()}})
     print(ch.getChannelTitle())
     cnt += 1
-    print('cnt : ' + str(cnt))
-    end = time.time()
-    t += round((end - start)/60, 2)
-    print('경과 시간 : ' + str(t) + '초')
+print('cnt : ' + str(cnt))
 del ch
 selectAll.close()
-
-
+endTime = time.time()
+print('%s seconds' %  (endTime - point2))
+print('total %s seconds' %  (endTime - startTime))
 
 
 
